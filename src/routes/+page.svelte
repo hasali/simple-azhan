c0d3c6eca6e6a5a96cd48fc36e75f16c16ae6b69
hasali <hali28.ent@gmail.com>
2026-03-04 00:34:18 -0500

    Properly deserializing data props from load function. Using setTimeout() to play audio on time.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,22 @@
     import Clock from '$lib/components/Clock.svelte'
     import { onMount } from 'svelte';
     
-    let { data }: PageProps = $props()
-    let time = $state(new Date());
-
-    let currentDate = new Date();
-    let seharTime = $derived<Date | undefined>(data.sehar)
-    let iftarTime = $derived<Date | undefined>(data.iftar)
+    const { data }: PageProps = $props()
+    
+    //need to convert the string array back to a an array of dates.
+    const prayerTimings: Date[] = $derived(data.timesList?.filter((t): t is string => !!t).map((t) => new Date(t)))
     
     let audio:any
-    let enabled = false
-    let targetTime = new Date(Date.now() + 60000)
-
     
-    
-    currentDate.setSeconds(0,0)
     onMount(() => {
-        const delay = targetTime.getTime() - Date.now()
-        const interval = setTimeout(() => {
-            audio.play()
-            // time = new Date();
-            // if ((seharTime?.getTime() == time.getTime()) || iftarTime?.getTime() == time.getTime()){
-            //     audio.play()
-            // }
-        }, delay);
-        
-        return () => clearInterval(interval); // Cleanup
+        for(let timing of prayerTimings ){
+            const delay = timing?.getTime() - Date.now()
+            const interval = setTimeout(() => {
+                audio.play()
+            }, delay);
+
+            return () => clearInterval(interval);
+        }    
     });
     
 </script>
@@ -36,9 +27,10 @@
 </header>
 <main>
     <Clock />
+    <br/>
     <div>
-        <h3>Sehar Time: {data.sehar?.toLocaleTimeString([], {timeStyle:'short'})}</h3>
-        <h3>Iftar Time: {data.iftar?.toLocaleTimeString([], {timeStyle:'short'})}</h3>
+        <h3>Sehar Time: {prayerTimings.at(0)?.toLocaleTimeString([], {timeStyle:'short'})}</h3>
+        <h3>Iftar Time: {prayerTimings.at(2)?.toLocaleTimeString([], {timeStyle:'short'})}</h3>
     </div>
     <audio bind:this={audio} src="/audio/01_ismail.mp3"></audio>
 </main>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,23 +1,19 @@
-import { shuffle } from '$lib/helpers/shuffler'
-import { tracks } from '$lib/tracks'
-import type { PageLoad } from './$types'
-import { getSunTimes } from '$lib/helpers/sun'
+import type { PageLoad } from './$types';
+import { getSunTimes } from '$lib/helpers/sun';
 
-export const load: PageLoad = async () => {
-    
-    const lat = 43.486167
-    const lon = -79.868157
-    const date = new Date()
-       
-    const times = getSunTimes(date, lat, lon) 
-    
-    //const tracklist = shuffle(tracks)
-    return{
-        
-        sehar: times.customDawn,
-        iftar: times.sunset
+export const load: PageLoad = () => {
+	const lat = 43.486167;
+	const lon = -79.868157;
+	const date = new Date();
 
-    } 
-}
+	const times = getSunTimes(date, lat, lon);
 
-
+	//load functions need to return data that is serializable
+    return {
+        timesList: [
+            times.customDawn?.toISOString(),
+            times.solarNoon?.toISOString(),
+            times.sunset?.toISOString()
+        ]
+    };
+};
