7d30d00e80d66347c0432ba0f66e95b835acaa6a
hasali <hali28.ent@gmail.com>
2026-03-05 18:46:56 -0500

    Using built in reactive svelte utilities like SvelteSet(). More research into deployment strats. Development env now uses proper certs.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
     import { shuffle } from '$lib/helpers/shuffler'
 	import { tracks } from '$lib/tracks';
     import Clock from '$lib/components/Clock.svelte'
+	import { SvelteDate, SvelteSet } from "svelte/reactivity";
     
     const { data }: PageProps = $props()
     let trackList = shuffle(tracks)
@@ -13,32 +14,45 @@
         (data.timesList ?? [])
         .filter((t): t is string => !!t)
         .map((t, i) => ({
-            time: new Date(t),
+            time: new SvelteDate(t),
             src: trackList[i % trackList.length].src
         }))
     )
  
+    let audioUnlocked = $state(false);
+
     
     let audio:HTMLAudioElement
-    let played = new Set()
+    let played = new SvelteSet()
+
+    const unlockAudio = () => {
+        audio.play().then(() => {
+            audio.pause();
+            audioUnlocked = true;
+        });
+    }
+    //let testDate = Date.now() + 60000;
     onMount(() => {
-        const interval = setInterval(() => {
+        const tick = () => {
             const now = Date.now()
             for(const timing of prayerTimings ){
                 const targetTime = timing?.time.getTime()
                 const diff = now - targetTime //so the exact time is past
                 //create a safety windows of 5 seconds before and 60 seconds after
-                
+                console.log(now, targetTime, diff)
                 if(diff >= -5000 && diff <= 60000 && (!played.has(targetTime))){ 
                     audio.src = timing.src              
                     audio?.play().catch(()=>{})
                     played.add(targetTime)
                 }
             }                    
-        }, 15000);
+        }
+
+        tick()
+        const interval = setInterval(tick, 15000)
         //midnight reset incase the app is never closed. 
-        const now = new Date();
-        const midnight = new Date();
+        const now = new SvelteDate();
+        const midnight = new SvelteDate();
         midnight.setHours(24, 0, 0, 0);
         const msUntilMidnight = midnight.getTime() - now.getTime();
 
@@ -60,9 +74,13 @@
     <br/>
     <div>
         <h3>Sehar Time: {prayerTimings.at(0)?.time.toLocaleTimeString([], {timeStyle:'short'})}</h3>
+        <h3>Zuhr Time: {prayerTimings.at(1)?.time.toLocaleTimeString([], {timeStyle:'short'})}</h3>
         <h3>Iftar Time: {prayerTimings.at(2)?.time.toLocaleTimeString([], {timeStyle:'short'})}</h3>
     </div>
-    
+    <br/>
+    {#if !audioUnlocked}
+    <button onclick={unlockAudio}>Enable Audio</button>
+{/if}
     <audio bind:this={audio}></audio>
     
 </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ 
+    server: {
+        https:{
+            cert: fs.readFileSync('localhost+3.pem'),
+            key: fs.readFileSync('localhost+3-key.pem')
+        },
+        host: true
+    },
+    plugins: [tailwindcss(), sveltekit()] 
+});
