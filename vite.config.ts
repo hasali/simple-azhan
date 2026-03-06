65cfc141d13b3689badd57a58dc45d1df6c28957
hasali <hali28.ent@gmail.com>
2026-03-06 15:05:14 -0500

    Issue: cloudflare build failing because of mkcert config in vite.config.ts. Removing server config setting to see if that works.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
-import fs from 'fs'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({ 
-    server: {
-        https:{
-            cert: fs.readFileSync('localhost+3.pem'),
-            key: fs.readFileSync('localhost+3-key.pem')
-        },
-        host: true
-    },
+   
     plugins: [tailwindcss(), sveltekit()] 
 });
